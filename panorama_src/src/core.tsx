0214bc56391475a1e6a3a064e6daf9b86f21a937
SpectralMorphy <75124358+SpectralMorphy@users.noreply.github.com>
2024-06-19 03:08:04 +0300

    .

--- panorama_src/src/core.tsx
+++ panorama_src/src/core.tsx
@@ -1,22 +1,97 @@
-// import lol from `util`
-// lol()
 import './panorama_adapter'
-import * as React from 'react'
+import 'react'
 import { render } from 'react-panorama'
 
-abstract class Lib {
-	
-	abstract name: string
+// -----------------------------------------------
+
+export interface LibModuleExports {
+	[field: string]: any
 }
 
-class Basis extends Lib {
+export interface LibModuleEnv {
+	lib: Lib
+	name: string
+	require: (module: string, options: LibRequireOptions) => LibModuleExports
+}
 
-	name = 'basis'
+export interface LibExecutableModule {
+	(env: LibModuleEnv): LibModuleExports | undefined
+}
+
+export interface LibPullOptions {
+
+}
+
+export interface LibRequireQueue {
+	(execute: () => void, modue: string): () => void
+}
+
+export interface LibRequireCallback {
+	(exports: LibModuleExports): void
+}
+
+export interface LibRequireOptions extends LibPullOptions{
+	callback: LibRequireCallback
+	queue: LibRequireQueue
 }
 
 // ----------------------------------------------------------------------------------------------
 
-const basis = new Basis()
+class LibQueuedState {
+}
+
+interface LibPullingState {
+}
+
+// ----------------------------------------------------------------------------------------------
+
+export class Lib {
+	
+	name: string
+	sources: {[module: string]: string} = {}
+	preload: {[module: string]: LibExecutableModule} = {}
+	loaded:  {[module: string]: LibModuleExports} = {}
+	private queued:  {[module: string]: LibQueuedState} = {}
+	private pulling:  {[module: string]: LibPullingState} = {}
+	
+	protected constructor(name: string){
+		this.name = name
+	}
+	
+	/**
+	!!!
+	*/
+	require(module: string, options: LibRequireOptions){
+		
+	}
+}
+
+// ----------------------------------------------------------------------------------------------
+
+
+const libs: {[key: string]: Lib} = {}
+
+class Basis extends Lib {
+
+	name = 'basis'	
+	
+	// -----------------------------------------------
+	
+	lib(name: string): Lib {
+		let lib = libs[name]
+		if(lib){
+			return lib
+		}
+		
+		lib = new Lib(name)
+		libs[name] = lib
+		return lib
+	}
+}
+
+// ----------------------------------------------------------------------------------------------
+
+// const basis = new Basis()
 
 
 
